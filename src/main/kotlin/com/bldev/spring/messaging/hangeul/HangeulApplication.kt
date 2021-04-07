d01e195d66468c02060620e605e16adfe41c8727
bldev2473 <bldev2473@gmail.com>
2021-04-07 23:51:41 +0900

    Initialize Spring project with IntelliJ

---
+++ src/main/kotlin/com/bldev/spring/messaging/hangeul/HangeulApplication.kt
@@ -0,0 +1,11 @@
+package com.bldev.spring.messaging.hangeul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HangeulApplication
+
+fun main(args: Array<String>) {
+    runApplication<HangeulApplication>(*args)
+}
---
+++ src/test/kotlin/com/bldev/spring/messaging/hangeul/HangeulApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bldev.spring.messaging.hangeul
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HangeulApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
